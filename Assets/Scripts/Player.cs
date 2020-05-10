ebd6964b9943a3cc0be212cab6f6e14edaf140c1
Sergio <bonfiglio.ser@gmail.com>
2020-05-10 19:50:33 +0100

    Test script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private bool privateBool;
+    [SerializeField]
+    private float privateFloat;
+    [SerializeField]
+    private string privateString;
+
+    // Read-only accessor Property
+    public bool PrivateBool
+    {
+        // Reading access
+        get
+        {
+            return privateBool;
+        }
+        // Writing access
+        private set
+        {
+            privateBool = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
